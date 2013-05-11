8d8b18c40f159181c09e2a9b0d9eeee4ae5ba9e4
Lea Marty <leanmarty@gmail.com>
2013-05-11 01:25:28 -0300

    Merge branch 'master' of D:\htdocs\wimble with conflicts.

---
+++ app/config/development/db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The development database settings. These get merged with the global settings.
+ */
+
+return array(
+    'default' => array(
+        'connection'  => array(
+            'dsn'        => 'mysql:host=spiraslhoes.dynalias.com:2221;dbname=wimble',
+            'username'   => 'dev',
+            'password'   => 'shoc2013',
+        ),
+    ),
+);
--- app/config/routes.php
+++ app/config/routes.php
@@ -4,4 +4,9 @@ return array(
     '_404_'   => 'welcome/404',    // The main 404 route
 
     'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+
+
+	'_root_'  => 'views/index',  // The default route
+	'_404_'   => 'views/404',    // The main 404 route
+	'views(/:any)' => 'views/index/$1'
 );
