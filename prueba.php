60008e5bdc73f8f556d4d5a06e6fd9bce488eb3a
lceballos <defyblur07>
2013-05-15 00:29:43 -0300

    Probando Hook

---
+++ prueba.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lceballos
+ * Date: 15/05/13
+ * Time: 00:28
+ * To change this template use File | Settings | File Templates.
+ */
+
+class SinRespetar {
+    function prueba(   $arg1   , $arg2   , $arg4){
+
+    }
+}
